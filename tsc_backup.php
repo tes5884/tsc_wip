9715a37e7aa2bc08d76f5791b61b0071037609c9
Tzvi Spitz <tes5884@gmail.com>
2015-05-11 18:06:49 -0400

    adding backup page

---
+++ tsc_backup.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<link rel="icon" href="favicon.ico" />
+		<title>TSpitz Consulting, Inc.</title>
+
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+		<link rel="stylesheet" href="css/main.css">
+	</head>
+	<body id="home">
+			<!-- navbar start -->
+			<?php include('php/navbar.php');?>
+			<!-- navbar end -->	
+    <div class="container">
+      <div style="margin-top: 10%" class="panel panel-default">
+        <div class="panel-body">
+          <h1>TSC Backup - Complete data protection solution</h1>
+          <p>Protecting your critical data has never been easier. Our automated cloud backup and recovery services will boost your productivity and reduce your time spent managing data loss.</p>
+          <p>Most businesses collect, retain, and transmit data on a daily basis that is critical to the success of their organization. The threat of data theft, natural disasters and hardware failures can result in a hard hit to a business’ bottom line, costing days of downtime, thousands of dollars and irreversible damage to a business’ reputation.</p>
+          <br />
+          <h2>Features and Platforms</h2>
+          <div class="row">
+            <div class="col-md-6">
+              <ul>
+                <li>Files & Folders</li>
+                <li>SQL Server</li>
+                <li>Exchange Server</li>
+                <li>System State</li>
+                <li>Image Backup</li>
+              </ul>
+            </div>
+            <div class="col-md-6">
+              <ul>
+                <li>Quick Start</li>
+                <p>Set up is fast.  And if you have a lot of data to backup initially, we can provide seeding services without tying up available bandwidth.</p>
+                <li>Automatic Backups</li>
+                <p>We can schedule your backups to happen whenever you want, as frequently as you want.</p>
+                <li>Manage Local and Offsite Backups</li>
+                <p>Our flexible solution allows for any of your data to be stored online and/or saved to local storage, eliminating the need for a separate local-only backup solution.</p>
+                <li>Military-grade encryption</li>
+                <p>Files are secured using 256-bit AES encryption and a private encryption key.</p>
+                <li>Off-site Storage</li>
+                <p>Data is stored in two, SSAE 16 Type II compliant datacenters with redundant storage and biometric security.</p>
+                <li>Robust, Flexible Archiving</li>
+                <p>Data-archiving settings allows historical data to be saved as long as you need and meet applicable compliance standards.</p>
+                <li>Multiple Revisions</li>
+                <p>Store an unlimited number of versions of the same file so there’s no fear of accidental deletion or overwriting a file.</p>
+                <li>Reporting and Notifications</li>
+                <p>We can send you notifications that your backups have completed successfully, including detailed statistics.</p>
+                <li>Multiple Device Support</li>
+                <p>Our solutions lets you back up a single computer or server, multiple computers, laptops or even multiple offices.</p>
+              </ul>
+            </div>
+          </div><!-- row -->
+        </div> <!-- panel body -->
+      </div> <!-- panel default -->
+    </div>
+		<?php include('php/footer.php');?>
+		<!-- js files -->	
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
+		<!-- end js files -->
+	</body>
+</html>
