bbc18870b363f1d4186590f2892f3fd5814e342b
Tzvi Spitz <tes5884@gmail.com>
2015-05-10 22:03:17 -0400

    new page

--- contact.php
+++ contact.php
@@ -13,8 +13,57 @@
 			<?php include('php/navbar.php');?>
 			<!-- navbar end -->	
     <div class="container">
-    
-    </div>
+      <div style="margin-top: 10%" class="row">
+        <div class="col-md-1"></div>
+        <div class="col-md-7">
+          <div class="panel panel-default">
+            <div class="panel-body">
+              <form name="contactform" method="post" action="php/mailer.php" class="form-horizontal" role="form">
+                <div class="form-group"><!-- name -->
+                  <label for="inputName" class="col-sm-2 control-label">Name</label>
+                  <div class="col-md-10">
+                    <input type="text" class="form-control" id="inputName" placeholder="Name">
+                  </div>
+                </div>
+                <div class="form-group"><!-- email -->
+                  <label for="inputEmail" class="col-sm-2 control-label">Email</label>
+                  <div class="col-sm-10">
+                    <input type="email" class="form-control" id="inputEmail" placeholder="Email">
+                  </div>
+                </div>
+                <div class="form-group"><!-- phone -->
+                  <label for="inputPhone" class="col-sm-2 control-label">Phone</label>
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" id="inputPhone" placeholder="xxx-xxx-xxxx">
+                  </div>
+                </div>  
+                <div class="form-group"><!-- message -->
+                  <label for="inputMessage" class="col-sm-2 control-label">Message</label>
+                  <div class="col-sm-10">
+                    <textarea class="form-control" rows="5"></textarea>
+                  </div>  
+                </div>
+                <button type="submit" class="btn btn-default">Submit</button>
+              </form>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-3">
+          <div class="panel panel-default">
+            <div class="panel-body">
+              <address>
+                <strong> TSpitz Consulting, Inc.</strong><br />
+                2 Olympia Lane <br />
+                Monsey, NY 10952 <br />
+                <abbr title="Phone">P:</abbr> (845) 219-1762 <br />
+                <abbr title="Email">E:</abbr> <a style="text-transform: lowercase" href="mailto:info@tspitzconsulting.com">info@tspitzconsulting.com</a>
+              </address>
+            </div>
+          </div>
+        </div>
+        <div class="col-md-1"></div>
+      </div> <!-- row -->
+    </div> <!-- container -->
 		<?php include('php/footer.php');?>
 		<!-- js files -->	
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
