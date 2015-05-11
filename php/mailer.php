29637b3c6726de5b29878c88a30263c9b721d4fb
Tzvi Spitz <tes5884@gmail.com>
2015-05-10 22:04:21 -0400

    mail script

---
+++ php/mailer.php
@@ -0,0 +1,40 @@
+<?php
+	/* Set e-mail recipient */
+	$myemail="info@tspitzconsulting.com";
+	/* Check all form inputs using check_input function */
+	$name=check_input($_POST['inputName'],"Your Name");
+  $email=check_input($_POST['inputEmail'],"Your E-mail Address");
+	$message=check_input($_POST['inputMessage'],"Your Message");
+	/* If e-mail is not valid show error message */
+	if(!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/",$email)){
+		show_error("Invalid e-mail address");
+	}
+	/* Let's prepare the message for the e-mail */
+	$subject="Someone has sent you a message";
+	$message="Someone has sent you a message using your contact form: \r\n \r\nName: $name - $email \r\nMessage:$message";
+	$headers='From: info@tspitzconsulting.com'."\r\n".'Reply-To: info@tspitzconsulting.com'."\r\n".'X-Mailer: PHP/'.phpversion();
+	/* Send the message using mail() function */
+	mail($myemail,$subject,$message,$headers);
+	/* Redirect visitor to the thank you page */
+	header('Location: http://rebshayala.org/test/index.html');
+	exit();
+	/* Functions we used */
+	function check_input($data,$problem=''){
+		$data=trim($data);
+		$data=stripslashes($data);
+		$data=htmlspecialchars($data);
+		if($problem && strlen($data)==0){
+			show_error($problem);
+		}
+		return $data;
+	}
+	
+	function show_error($myError){
+	?>
+		<html><body><p>Please correct the following error:</p><strong><?php
+		echo $myError;
+	?>
+		</strong><p>Hit the back button and try again</p></body></html><?php
+		exit();
+	}
+?>
